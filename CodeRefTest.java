b72879084844ca6bf3083dfd6dbe0241ffce06be
Qingchuan Zhu <zqc@utexas.edu>
2018-09-25 22:13:05 -0700

    Added method for filtering apple in prior J8 way

--- CodeRefTest.java
+++ CodeRefTest.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Apple {
 	private String color = "";
 
@@ -11,5 +13,31 @@ class Apple {
 
 	String getColor() {
 		return color;
+	}
+
+	public String toString() {
+		return color +" apple";
+	}
+}
+
+class CodeRefTest {
+	public static List<Apple> fileterGreenApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if ("green".equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Apple> apples = new ArrayList<>();
+		apples.add(new Apple("green"));
+		apples.add(new Apple("red"));
+		apples.add(new Apple("blue"));
+
+		List<Apple> result = CodeRefTest.fileterGreenApples(apples);
+		System.out.println(result);
 	}
 }
