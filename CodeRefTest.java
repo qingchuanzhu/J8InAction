0a40e50a7188a065c9ad47449c51f20087de56e9
Qingchuan Zhu <zqc@utexas.edu>
2018-09-26 13:52:38 -0700

    Added heavy Apple filter

--- CodeRefTest.java
+++ CodeRefTest.java
@@ -2,17 +2,24 @@ import java.util.*;
 
 class Apple {
 	private String color = "";
+	private double weight = 0;
 
 	Apple () {
 		color = "";
+		weight = 0;
 	}
 
-	Apple (String col){
+	Apple (String col, double w){
 		color = col;
+		weight = w;
 	}
 
 	String getColor() {
 		return color;
+	}
+
+	double getWeight() {
+		return weight;
 	}
 
 	public String toString() {
@@ -31,13 +38,25 @@ class CodeRefTest {
 		return result;
 	}
 
+	public static List<Apple> filterHeavyApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if (apple.getWeight() > 150) {
+				result.add(apple);
+			}
+		}
+		return result;	
+	}
+
 	public static void main(String[] args) {
 		ArrayList<Apple> apples = new ArrayList<>();
-		apples.add(new Apple("green"));
-		apples.add(new Apple("red"));
-		apples.add(new Apple("blue"));
+		apples.add(new Apple("green", 134));
+		apples.add(new Apple("red", 149));
+		apples.add(new Apple("blue", 187));
 
-		List<Apple> result = CodeRefTest.fileterGreenApples(apples);
+		List<Apple> result = fileterGreenApples(apples);
+		List<Apple> result2 = filterHeavyApples(apples);
 		System.out.println(result);
+		System.out.println(result2);
 	}
 }
