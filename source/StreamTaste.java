882316f1f545997e66b2b1d59c861ffd8eea202e
Qingchuan Zhu <zqc@utexas.edu>
2018-10-02 22:02:56 -0700

    Added prints to show how Stream works underneath

--- source/StreamTaste.java
+++ source/StreamTaste.java
@@ -10,5 +10,25 @@ class StreamTaste {
 					.limit(3)
 					.collect(toList());
 		System.out.println(threeHighCaloricDishName);
+
+		List<String> names = new ArrayList<>();
+		Iterator<Dish> iterator = Dish.menu.iterator();
+		while(iterator.hasNext()){
+			Dish d = iterator.next();
+			names.add(d.getName());
+		}
+
+		List<String> names_s = Dish.menu.stream()
+										.filter(d -> {
+											System.out.println("filtering " + d.getName());
+											return d.getCalories() > 300;
+										})
+										.map(d -> {
+											System.out.println("mapping " + d.getName());
+											return d.getName();
+										})
+										.limit(3)
+										.collect(toList());
+		System.out.println(names_s);
 	}
 }
