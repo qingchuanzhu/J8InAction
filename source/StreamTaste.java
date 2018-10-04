accad669e3d1be5cb4b13c5939a21776d48a2790
Qingchuan Zhu <zqc@utexas.edu>
2018-10-03 20:56:29 -0700

    Finished the 5.1 filtering section for streams

--- source/StreamTaste.java
+++ source/StreamTaste.java
@@ -30,5 +30,33 @@ class StreamTaste {
 										.limit(3)
 										.collect(toList());
 		System.out.println(names_s);
+
+		//5.1.1 Filtering with Predicate
+		List<Dish> vegetarianMenu = Dish.menu.stream()
+											.filter(Dish::isVegetarian)
+											.collect(toList());
+		System.out.println(vegetarianMenu);
+
+		// 5.1.2 Filtering unique elements
+		List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+		numbers.stream()
+				.filter((a) -> a%2 == 0)
+				.distinct()
+				.forEach(System.out::println);
+
+		// 5.1.4 Skipping elements
+		List<Dish> skip2HighCaloricDishName = Dish.menu.stream()
+													.filter(d -> d.getCalories() > 300)
+													.skip(2)
+													.collect(toList());
+		System.out.println(skip2HighCaloricDishName);
+
+		// Quiz 5.1
+		List<Dish> first2MeatDishName = Dish.menu.stream()
+													.filter(d -> d.getType() == Dish.Type.MEAT)
+													.limit(2)
+													.collect(toList());
+		System.out.println(first2MeatDishName);
+
 	}
 }
