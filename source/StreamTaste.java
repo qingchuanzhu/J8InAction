721532806a9cf481e9950e6bee275ed564673822
Qingchuan Zhu <zqc@utexas.edu>
2018-10-05 18:33:27 -0700

    Finished the 5.2 Mapping of Stream

--- source/StreamTaste.java
+++ source/StreamTaste.java
@@ -56,7 +56,56 @@ class StreamTaste {
 													.filter(d -> d.getType() == Dish.Type.MEAT)
 													.limit(2)
 													.collect(toList());
-		System.out.println(first2MeatDishName);
+		System.out.println("Quiz 5.1 answer " + first2MeatDishName);
 
+		// 5.2.1 map
+		List<String> words = Arrays.asList("Java8", "Lambdas", "In", "Action");
+		List<Integer> numberOfChar = words.stream()
+											.map(String::length)
+											.collect(toList());
+		System.out.println("5.2.1 map " + numberOfChar);
+
+		// 5.2.2 flap map
+		List<String> distinctChar = words.stream()                             // Stream<String>
+											.map(word -> word.split(""))       // Stream<String[]>
+											.flatMap(Arrays::stream)           // Stream<String> instead of Stream<Stream<String>>
+											.distinct()
+											.collect(toList());
+		System.out.println("5.2.2 flat map " + distinctChar);
+
+		// Quiz 5.2.1
+		List<Integer> squaredNum = Arrays.asList(1, 2, 3, 4, 5).stream()
+																.map(a -> a * a)
+																.collect(toList());
+		System.out.println("Quiz 5.2.1 answer = " + squaredNum);
+
+		//Quiz 5.2.2
+		System.out.println("Quiz 5.2.2 answer = ");
+		Arrays.asList(1, 2, 3).stream()
+							  .map(a -> new int[][]{{a, 3}, {a, 4}})  // Stream<Integer[][]>
+							  .flatMap(Arrays::stream)  // Stream<Integer[]>
+							  .forEach(pair -> System.out.println("[" + pair[0] + "," + pair[1] + "]"));
+		// Quiz 5.2.2 using two map
+		List<Integer> numbers1 = Arrays.asList(1,2,3);
+		List<Integer> numbers2 = Arrays.asList(3,4);
+		List<int[]> permutation = numbers1.stream()
+											.flatMap(a -> numbers2.stream()
+															  .map(b -> new int[]{a, b})) // Stream<Int[]>
+											.collect(toList());
+		System.out.println("Quiz 5.2.2 answer = ");
+		for (int[] pair : permutation) {
+			System.out.println("[" + pair[0] + "," + pair[1] + "]");
+		}
+
+		// Quiz 5.2.3
+		List<int[]> permutationBy3 = numbers1.stream()
+											 .flatMap(a -> numbers2.stream()
+											 						.map(b -> new int[]{a, b}))
+											 .filter(pair -> (pair[0] + pair[1])%3 == 0)
+											 .collect(toList());
+		System.out.println("Quiz 5.2.3 answer = ");
+		for (int[] pair : permutationBy3) {
+			System.out.println("[" + pair[0] + "," + pair[1] + "]");
+		}
 	}
 }
