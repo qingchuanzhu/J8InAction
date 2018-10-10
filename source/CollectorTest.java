6a12dbbffe4e2d7e930a9c6e49decf3b49669cf4
Qingchuan Zhu <zqc@utexas.edu>
2018-10-10 16:47:43 -0700

    Finished the 6.2 reducing part

--- source/CollectorTest.java
+++ source/CollectorTest.java
@@ -1,10 +1,28 @@
+import java.util.*;
 import java.util.stream.*;
-import java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.*;
 
 class CollectorTest {
 	public static void main(String[] args) {
 		// 6.2.0 count
 		long howManyDishes = Dish.menu.stream().collect(Collectors.counting());
 		System.out.println("Number of dishes: " + howManyDishes);
+
+		// 6.2.1 get max min
+		Optional<Dish> mostCalorieDish = Dish.menu.stream()
+												  .collect(maxBy(Comparator.comparing(Dish::getCalories)));
+		System.out.println("Most Calorie Dish: " + mostCalorieDish);
+
+		// 6.2.2 Summarization
+		int totalCalories = Dish.menu.stream()
+									 .collect(summingInt(Dish::getCalories));
+		double avgCalories = Dish.menu.stream()
+									  .collect(averagingInt(Dish::getCalories));
+		System.out.println("Total Calorie is " + totalCalories + ", Average Calorie is " + avgCalories);
+
+		// Joining strings
+		String shortMenu = Dish.menu.stream()
+									.map(Dish::getName).collect(joining(", "));
+		System.out.println("Short Menu is: " + shortMenu);
 	}
 }
