6782aba2efba0b0790e3c34543a6db74922dc50c
Qingchuan Zhu <zqc@utexas.edu>
2018-10-12 15:10:24 -0700

    Created the custom Collector mimic Collector returned by toList()

---
+++ source/ToListCollector.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.function.*;
+import java.util.stream.Collector;
+import static java.util.stream.Collector.Characteristics.*;
+
+class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+	
+}
